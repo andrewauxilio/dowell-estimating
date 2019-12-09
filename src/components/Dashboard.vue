b336fc8f093fcd4c199be105553c8c22e829402d
andrew.auxilio@gmail.com <andrew.auxilio@gmail.com>
2019-12-09 13:04:17 +1100

    temp fix for current date for 3 day chart

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -105,7 +105,7 @@
         </div>
     </div>
 
-    <div class="fab-container">
+    <div v-if="!loading" class="fab-container">
         <button class="buttons" tooltip="Download Reports" data-toggle="modal" data-target=".report-modal">
             <i class="fab fas fa-download"></i>
         </button>
@@ -294,3 +294,37 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.loader {
+    border: 16px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 16px solid #3498db;
+    width: 120px;
+    height: 120px;
+    -webkit-animation: spin 2s linear infinite;
+    /* Safari */
+    animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+    }
+
+    100% {
+        -webkit-transform: rotate(360deg);
+    }
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- src/components/chart-data/7days.vue
+++ src/components/chart-data/7days.vue
@@ -107,55 +107,98 @@ export default {
 
     methods: {
         async fetchData() {
-            let dateDifference = this.endDate - this.startDate
+            let checkDate = moment().format("YYYYMMDD")
+            console.log(checkDate)
+            console.log(this.endDate)
+            if (!(this.endDate == checkDate)) {
+                let dateDifference = this.endDate - this.startDate
+                let endDate2 = moment(this.startDate).subtract(1, "days").format("YYYYMMDD")
+                let startDate2 = moment(endDate2).subtract(dateDifference, "days").format("YYYYMMDD")
+                let endDate3 = moment(startDate2).subtract(1, "days").format("YYYYMMDD")
+                let startDate3 = moment(endDate3).subtract(dateDifference, "days").format("YYYYMMDD")
 
-            let endDate2 = moment(this.startDate).subtract(1, "days").format("YYYYMMDD")
-            let startDate2 =  moment(endDate2).subtract(dateDifference, "days").format("YYYYMMDD")
+                console.log(dateDifference)
+                this.loading = true;
+                await axios
+                    .all([
+                        axios.post("/estimating/kpis", {
+                            end: this.endDate,
+                            start: this.startDate,
+                            site: this.site,
+                            grouped: 0
+                        }),
 
-            let endDate3 = moment(startDate2).subtract(1, "days").format("YYYYMMDD")
-            let startDate3 = moment(endDate3).subtract(dateDifference, "days").format("YYYYMMDD")
+                        axios.post("/estimating/kpis", {
+                            end: endDate2,
+                            start: startDate2,
+                            site: this.site,
+                            grouped: 0
+                        }),
 
-            console.log(dateDifference) 
-            this.loading = true;
-            await axios
-                .all([
-                    axios.post("/estimating/kpis", {
-                        end: this.endDate,
-                        start: this.startDate,
-                        site: this.site,
-                        grouped: 0
-                    }),
+                        axios.post("/estimating/kpis", {
+                            end: endDate3,
+                            start: startDate3,
+                            site: this.site,
+                            grouped: 0
+                        })
+                    ])
+                    .then(responseArr => {
+                        this.siteData1 = responseArr[0].data;
+                        this.siteData2 = responseArr[1].data;
+                        this.siteData3 = responseArr[2].data;
 
-                    axios.post("/estimating/kpis", {
-                        end: endDate2,
-                        start: startDate2,
-                        site: this.site,
-                        grouped: 0
-                    }),
+                        console.log(this.siteData1);
+                        console.log(this.siteData2);
+                        console.log(this.siteData3);
 
-                    axios.post("/estimating/kpis", {
-                        end: endDate3,
-                        start: startDate3,
-                        site: this.site,
-                        grouped: 0
-                    })
-                ])
-                .then(responseArr => {
-                    this.siteData1 = responseArr[0].data;
-                    this.siteData2 = responseArr[1].data;
-                    this.siteData3 = responseArr[2].data;
+                        this.fillData();
+                        this.loading = false;
+                    });
+            } else {
+                this.loading = true;
+                await axios
+                    .all([
+                        axios.post("/estimating/kpis", {
+                            end: this.endDate,
+                            start: this.startDate,
+                            site: this.site,
+                            grouped: 0
+                        }),
+                        axios.post("/estimating/kpis", {
+                            end: moment(this.endDate)
+                                .subtract(4, "days")
+                                .format("YYYYMMDD"),
+                            start: moment(this.startDate)
+                                .subtract(4, "days")
+                                .format("YYYYMMDD"),
+                            site: this.site,
+                            grouped: 0
+                        }),
+                        axios.post("/estimating/kpis", {
+                            end: moment(this.endDate)
+                                .subtract(5, "days")
+                                .format("YYYYMMDD"),
+                            start: moment(this.startDate)
+                                .subtract(5, "days")
+                                .format("YYYYMMDD"),
+                            site: this.site,
+                            grouped: 0
+                        })
+                    ])
+                    .then(responseArr => {
+                        this.siteData1 = responseArr[0].data;
+                        this.siteData2 = responseArr[1].data;
+                        this.siteData3 = responseArr[2].data;
 
-                    console.log(responseArr[0].data);
-                    console.log(responseArr[1].data);
-                    console.log(responseArr[2].data);
+                        console.log(this.siteData1);
+                        console.log(this.siteData2);
+                        console.log(this.siteData3);
 
-                    console.log(this.siteData1);
-                    console.log(this.siteData2);
-                    console.log(this.siteData3);
+                        this.fillData();
+                        this.loading = false;
+                    });
+            }
 
-                    this.fillData();
-                    this.loading = false;
-                });
         },
 
         fillData() {
@@ -163,11 +206,11 @@ export default {
             let day2 = this.siteData2;
             let day3 = this.siteData3;
             this.datacollection = {
-                labels: [   
-                            moment(day3[0].DATE_FROM).format('MMM DD YY') + " - " + moment(day3[0].DATE_TO).format('MMM DD YY'), 
-                            moment(day2[0].DATE_FROM).format('MMM DD YY') + " - " + moment(day2[0].DATE_TO).format('MMM DD YY'),
-                            moment(day1[0].DATE_FROM).format('MMM DD YY') + " - " + moment(day1[0].DATE_TO).format('MMM DD YY'),  
-                        ],
+                labels: [
+                    moment(day3[0].DATE_FROM).format('MMM DD YY') + " - " + moment(day3[0].DATE_TO).format('MMM DD YY'),
+                    moment(day2[0].DATE_FROM).format('MMM DD YY') + " - " + moment(day2[0].DATE_TO).format('MMM DD YY'),
+                    moment(day1[0].DATE_FROM).format('MMM DD YY') + " - " + moment(day1[0].DATE_TO).format('MMM DD YY'),
+                ],
                 datasets: [{
                         label: "Quotes",
                         backgroundColor: "rgba(75, 192, 192, 0.7)",
